2eb402c966ff2e7349c2be39c375c7d861d1b770
@hexlet/cli <support@hexlet.io>
2022-10-02 17:09:14 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,42 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Locale;
+// BEGIN
+public class App {
+    public static void main(String[] args) {
+        scrabble("shaaaaao", "sho");
+    }
+    public static boolean scrabble(String symbols, String word) {
+        String lowerSymbols = symbols.toLowerCase(Locale.ROOT);
+        String lowerWord = word.toLowerCase(Locale.ROOT);
+        List<String> listOfSymbols = new ArrayList<>();
+        List<String> listOfWords = new ArrayList<>();
+        List<String> newList = new ArrayList<>();
+        for (int i = 0; i < lowerSymbols.length(); i++) {
+            listOfSymbols.add(String.valueOf(lowerSymbols.charAt(i)));
+        }
+        for (int x = 0; x < lowerWord.length(); x++ ) {
+            listOfWords.add(String.valueOf(lowerWord.charAt(x)));
+        }
+        int countOfSymbols = 0;
+        for (int r = 0; r < lowerWord.length(); r++) {
+            if (listOfWords.get(r).equals(listOfSymbols.get(countOfSymbols))) {
+                newList.add(listOfSymbols.get(countOfSymbols));
+                countOfSymbols = 0;
+            } else if (!(listOfWords.get(r).equals(listOfSymbols.get(countOfSymbols)))) {
+                countOfSymbols +=1;
+                r -=1;
+            }
+        }
+        for (int s = 0; s < lowerWord.length(); s++) {
+            if (!(newList.get(s).equals(listOfWords.get(s)))) {
+                return false;
+            }
+        }
+            return true;
+    }
+}
+
+//END
