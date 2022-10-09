3c0c53c07f06df932972318111246470257c48fa
@hexlet/cli <support@hexlet.io>
2022-10-09 21:44:41 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -21,7 +21,7 @@ public class App {
         for (int r = 0; r < lowerWord.length(); r++) {
             if (listOfWords.get(r).equals(listOfSymbols.get(countOfSymbols))) {
                 newList.add(listOfSymbols.get(countOfSymbols));
-                listOfSymbols.remove(r);
+                listOfSymbols.remove(countOfSymbols);
                 countOfSymbols = 0;
             } else if (!(listOfWords.get(r).equals(listOfSymbols.get(countOfSymbols)))) {
                 countOfSymbols +=1;
