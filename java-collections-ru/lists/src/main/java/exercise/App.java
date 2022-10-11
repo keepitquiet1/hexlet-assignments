0f135a54087c4c8c3e875137e8fbf3df9178535b
@hexlet/cli <support@hexlet.io>
2022-10-11 09:53:12 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -3,6 +3,7 @@ package exercise;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.List;
 // BEGIN
 public class App {
     public static boolean scrabble(String symbols, String word) {
