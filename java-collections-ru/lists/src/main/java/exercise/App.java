e64a88a0c42e481811c5657e4aac34ca83e7fff1
@hexlet/cli <support@hexlet.io>
2022-10-26 11:34:12 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -8,24 +8,25 @@ import java.util.List;
 public class App {
 public static boolean scrabble(String symbols, String word){
         String lowerWord = word.toLowerCase(Locale.ROOT);
+        String lowerSymbols = symbols.toLowerCase(Locale.ROOT);
         int countOfSymbols = 0;
         String newWord = "";
         for (int r = 0; r < lowerWord.length(); r++) {
-                String lowerSymbols = symbols.toLowerCase(Locale.ROOT);
-                if (countOfSymbols > lowerSymbols.length()) {
+                if (countOfSymbols >= lowerSymbols.length()) {
                         return false;
                 }
                 else if (lowerWord.charAt(r)==(lowerSymbols.charAt(countOfSymbols))) {
-                        String hlowerSymbols = lowerSymbols.replaceAll(String.valueOf(lowerSymbols.charAt(countOfSymbols)), "");
+                        String hlowerSymbols = lowerSymbols.replaceAll(String.valueOf(countOfSymbols), "");
                         newWord = newWord + lowerSymbols.charAt(countOfSymbols);
                         countOfSymbols = 0;
                         lowerSymbols = hlowerSymbols;
                 } else if (lowerWord.charAt(r)!=(lowerSymbols.charAt(countOfSymbols))) {
                         countOfSymbols += 1;
                         r -= 1;
-                } System.out.println(lowerWord);
-                System.out.println(newWord);
-                System.out.println(lowerSymbols);
+                } System.out.println("1" + lowerWord);
+                System.out.println("2" + newWord);
+                System.out.println("3" + lowerSymbols);
+                System.out.println("счет" + countOfSymbols);
         }
 
         for (int s = 0; s < lowerWord.length(); s++) {
