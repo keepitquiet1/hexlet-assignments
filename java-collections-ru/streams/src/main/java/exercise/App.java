b6be81533819fbba0ad01c8b19f7a95eb074365d
@hexlet/cli <support@hexlet.io>
2022-11-15 13:03:48 +0300

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 public class App {
     public static int getCountOfFreeEmails(List<String> emails) {
         int count = 0;
-        String gmail = "gmail.com";
-        String ya = "yandex.ru";
-        String hot = "hotmail.com";
+        String gmail = "@gmail.com";
+        String ya = "@yandex.ru";
+        String hot = "@hotmail.com";
         for (int i = 0; i<emails.size(); i++) {
             if (emails.get(i).contains(gmail)){
                 count +=1;
