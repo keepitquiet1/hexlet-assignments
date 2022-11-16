99283fb5463e4251910039507eefb44b46e95dac
@hexlet/cli <support@hexlet.io>
2022-11-16 17:05:56 +0300

    submit java-collections-ru/advanced-streams

---
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -0,0 +1,13 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+// BEGIN
+public class {
+public static String getForwardedVariables(String text) {
+        return "&";
+    }
+}
+
+//END
