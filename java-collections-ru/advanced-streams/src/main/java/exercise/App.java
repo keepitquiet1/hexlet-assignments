9fc0978549975e1b604b4b68a8157cb0f9dfd20f
@hexlet/cli <support@hexlet.io>
2022-11-17 16:15:05 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,4 +1,4 @@
- package exercise;
+package exercise;
 
 import java.util.stream.Collectors;
 import java.util.Arrays;
@@ -6,20 +6,20 @@ import java.util.Arrays;
 // BEGIN
 public class App {
     public static String getForwardedVariables(String confFile) {
-            List<String> resultList = Arrays.stream(confFile.split("\n"))
-                    .filter(str -> str.startsWith("environment"))
-                    .map(obj -> obj.replace("environment=", ""))
-                    .map(d -> d.replace("\"", ""))
-                    .map(l -> l.replace(" ", ""))
-                    .collect(Collectors.toList());
+        List<String> resultList = Arrays.stream(confFile.split("\n"))
+            .filter(str -> str.startsWith("environment"))
+            .map(obj -> obj.replace("environment=", ""))
+            .map(d -> d.replace("\"", ""))
+            .map(l -> l.replace(" ", ""))
+            .collect(Collectors.toList());
             var resultListToString = resultList.toString();
-            List<String> resList = Arrays.stream(resultListToString
-                    .split(","))
-                    .filter(s -> s.contains("X_FORWARDED_"))
-                    .map(s -> s.replace("X_FORWARDED_", ""))
-                    .collect(Collectors.toList());
-            System.out.println(resList);
-            return resList.toString();
+        List<String> resList = Arrays.stream(resultListToString
+            .split(","))
+            .filter(s -> s.contains("X_FORWARDED_"))
+            .map(s -> s.replace("X_FORWARDED_", ""))
+            .collect(Collectors.toList());
+        System.out.println(resList);
+        return resList.toString();
         }
     }
 
