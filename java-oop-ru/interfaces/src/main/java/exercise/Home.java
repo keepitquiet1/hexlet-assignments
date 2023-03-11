083f13e843a67c4b3ac0dd745487a834308f00af
@hexlet/cli <support@hexlet.io>
2023-03-11 14:15:47 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/Home.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Home.java
@@ -1,5 +1,18 @@
 package exercise;
 
 // BEGIN
+public interface Home {
+    double getArea();
+
+    default int compareTo(Home another){
+        if (this.getArea() >  another.getArea()){
+            return 1;
+        }
+        if (this.getArea() < another.getArea()){
+            return -1;
+        }
+        return 0;
+    }
+}
 
 // END
--- java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
+++ java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
@@ -57,19 +57,6 @@ public class ReversedSequence implements CharSequence{
 //        }
 //        this.revStr = res.toString();
 //    }
-    public interface Home {
-    double getArea();
-
-    default int compareTo(Home another){
-        if (this.getArea() >  another.getArea()){
-            return 1;
-        }
-        if (this.getArea() < another.getArea()){
-            return -1;
-        }
-        return 0;
-    }
-}
 }
 
 // END
