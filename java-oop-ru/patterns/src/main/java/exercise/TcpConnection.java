04eee974374397db2930aafc400b0b3bf8c07bc3
@hexlet/cli <support@hexlet.io>
2023-03-14 21:31:17 +0300

    submit java-oop-ru/patterns

--- java-oop-ru/patterns/src/main/java/exercise/TcpConnection.java
+++ java-oop-ru/patterns/src/main/java/exercise/TcpConnection.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 
 // BEGIN
 public class TcpConnection {
+String getCurrentState();
+    void connect();
+    void disconnect();
+    void write(String str);
     private Connection connection;
 
     public TcpConnection(String ip, int port) {
         this.connection = new Disconnected(this);
     }
+    @Override
     public String getCurrentState() {
         return this.connection.state();
     }
@@ -17,20 +22,22 @@ public class TcpConnection {
         this.connection = connection;
     }
 
+    @Override
     public void connect() {
         setConnection(new Connected(this));
         this.connection.connect();
     }
 
+    @Override
     public void disconnect() {
         setConnection(new Disconnected(this));
         this.connection.disconnect();
     }
 
+    @Override
     public void write(String str) {
         this.connection.write(str);
     }
 
 }
-
 // END
