a407b981d5d0c7fa93b46e707ff71bec61fb8e13
@hexlet/cli <support@hexlet.io>
2023-03-14 12:45:35 +0300

    submit java-oop-ru/patterns

--- java-oop-ru/patterns/src/main/java/exercise/connections/Connection.java
+++ java-oop-ru/patterns/src/main/java/exercise/connections/Connection.java
@@ -2,12 +2,9 @@ package exercise.connections;
 
 public interface Connection {
     // BEGIN
-    public interface Connection {
     void connect();
     void disconnect();
     void write(String str);
     String state();
-
-}
     // END
 }
