00bcebe907264c48d9e811ad8170188400c31309
@hexlet/cli <support@hexlet.io>
2023-03-16 20:37:48 +0300

    submit java-oop-ru/patterns_16.03.23_20.34.44

---
+++ java-oop-ru/patterns_16.03.23_20.34.44/src/main/java/exercise/TcpConnection.java
@@ -0,0 +1,39 @@
+package exercise;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TCP;
+// BEGIN
+public class TcpConnection implements  TCP {
+    private Connection connection;
+
+    public TcpConnection(String ip, int port) {
+        this.connection = new Disconnected(this);
+    }
+    @Override
+    public String getCurrentState() {
+        return this.connection.state();
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void connect() {
+        setConnection(new Connected(this));
+        this.connection.connect();
+    }
+
+    @Override
+    public void disconnect() {
+        setConnection(new Disconnected(this));
+        this.connection.disconnect();
+    }
+
+    @Override
+    public void write(String str) {
+        this.connection.write(str);
+    }
+
+}
+// END
---
+++ java-oop-ru/patterns_16.03.23_20.34.44/src/main/java/exercise/connections/Connected.java
@@ -0,0 +1,33 @@
+package exercise.connections;
+import java.util.Connection;
+import exercise.java.util.TcpConnection;
+// BEGIN
+public class Connected implements  Connection {
+    private TcpConnection connection;
+
+    public Connected(TcpConnection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public String state() {
+        return "connected";
+    }
+
+    @Override
+    public void connect() {
+        System.out.println("Error! Connection is already connected!");
+    }
+
+    @Override
+    public void disconnect() {
+        System.out.println("disconnected");
+
+    }
+
+    @Override
+    public void write(String str) {
+        System.out.println("wrote");
+    }
+}
+// END
