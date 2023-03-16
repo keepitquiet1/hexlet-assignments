59f1f872a6924846b2a0a466ae57c6a573eafcb5
@hexlet/cli <support@hexlet.io>
2023-03-16 20:53:01 +0300

    submit java-oop-ru/patterns

--- java-oop-ru/patterns/src/main/java/exercise/connections/Connected.java
+++ java-oop-ru/patterns/src/main/java/exercise/connections/Connected.java
@@ -1,5 +1,33 @@
 package exercise.connections;
-
+import exercise.java.util.TcpConnection;
+import java.util.Connection;
 // BEGIN
+public class Connected implements  Connection {
+    private TcpConnection connection;
 
+    public Connected(TcpConnection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public String state() {
+        return "connected";
+    }
+
+    @Override
+    public void connect() {
+        System.out.println("Error! Connection is already connected!");
+    }
+
+    @Override
+    public void disconnect() {
+        System.out.println("disconnected");
+
+    }
+
+    @Override
+    public void write(String str) {
+        System.out.println("wrote");
+    }
+}
 // END
